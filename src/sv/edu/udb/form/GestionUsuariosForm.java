bc5fbd1be45c8df7e37188e4b8f591d0be881568
JN <jehudi.aguilar@gmail.com>
2025-11-17 19:54:54 -0600

    Corregido nombres de paquetes e imports

--- src/sv/edu/udb/form/GestionUsuariosForm.java
+++ src/sv/edu/udb/form/GestionUsuariosForm.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package form;
+package sv.edu.udb.form;;
 
-import conexion.Conexion;
+import sv.edu.udb.conexion.Conexion;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
